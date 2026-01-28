d294fef21458a2b45d0ecd1c8ce4cddf320597ec
j2wicee <joshuaevans2x@gmail.com>
2026-01-28 15:42:08 -0500

    Initial Commit

---
+++ Fitted/FittedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FittedApp.swift
+//  Fitted
+//
+//  Created by Joshua  Evans  on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct FittedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
